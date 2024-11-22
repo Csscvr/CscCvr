352a256e9225339c9feaf478cc5a69e966d346c7
Csscvr <csscvr.spprt@gmail.com>
2024-11-22 10:46:20 +0100

    Update passwordTesterjs.js

--- jscode/passwordTesterjs.js
+++ jscode/passwordTesterjs.js
@@ -9,9 +9,9 @@ function testPasswordStrength() {
   let strength = 0;
 
   // Check password length
-  if (password.length < 8) {
+  if (password.length < 10) {
     strength = 0;
-  } else if (password.length >= 8 && password.length < 12) {
+  } else if (password.length >= 10 && password.length < 15) {
     strength = 1;
   } else {
     strength = 2;
@@ -24,17 +24,17 @@ function testPasswordStrength() {
 
   // Check for lowercase letters
   if (/[a-z]/.test(password)) {
-    strength++;
+    strength = strength + 0.5;
   }
 
   // Check for numbers
   if (/\d/.test(password)) {
-    strength++;
+    strength = strength + 0.5;
   }
 
   // Check for special characters
   if (/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
-    strength++;
+    strength = strength + 0.5;
   }
 
   // Update strength meter and message
@@ -48,29 +48,48 @@ function updateStrengthMeter(strength) {
   switch (strength) {
     case 0:
       meterWidth = 20;
-      message = 'Very weak';
+      message = 'Extreamly weak';
       strengthMeterFill.className = 'strength-meter-fill weak';
       break;
     case 1:
+      meterWidth = 30;
+      message = 'Very Weak
+      strengthMeterFill.className = 'strength-meter-fill weak'
+      break;
+    case 2:
       meterWidth = 40;
       message = 'Weak';
       strengthMeterFill.className = 'strength-meter-fill weak';
       break;
-    case 2:
-      meterWidth = 60;
-      message = 'Medium';
+    case 3:
+      meterWidth = 50;
+      message = '50% Below Medium';
       strengthMeterFill.className = 'strength-meter-fill medium';
       break;
-    case 3:
-      meterWidth = 80;
-      message = 'Strong';
-      strengthMeterFill.className = 'strength-meter-fill strong';
-      break;
     case 4:
-      meterWidth = 100;
-      message = 'Very strong';
+      meterWidth = 60;
+      message = 'Medium'
+      strengthMeterFill.className = 'strength-meter-fill medium'
+      break;
+    case 5:
+      meterWidth = 70;
+      message = ' 50% Below Strong';
       strengthMeterFill.className = 'strength-meter-fill strong';
       break;
+    case 6:
+      meterWidth = 80;
+      message = 'strong';
+      strengthMeterFill.className = 'strength-meter-fill strong';
+      break;
+    case 7 :
+      meterWidth = 90;
+      message = '50% Below Very Strong'
+      strengthMeterFill.className = 'strength-meter-fill strong+'
+      break;
+    case 8:
+      meterWidth = 100;
+      message = 'Very Strong'
+      strengthMeterFill.className = 'strength-meter-fill strong+'
   }
 
   strengthMeterFill.style.width = `${meterWidth}%`;
